56719b78ef6020e80fd77cb34633474817057b59
Gil <gil@localhost.localdomain>
2019-07-31 21:38:47 -0700

    web packed and minified, prod build ready

---
+++ build/precache-manifest.7c102353135ad09f599846a2592d62e5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78795846514a7427633b51bf4285887e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9c1af1717caab288d3ac",
+    "url": "/static/css/main.e660011b.chunk.css"
+  },
+  {
+    "revision": "16d558a4ee3fc9120da8",
+    "url": "/static/js/2.d405fc88.chunk.js"
+  },
+  {
+    "revision": "9c1af1717caab288d3ac",
+    "url": "/static/js/main.e47cc8c4.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
